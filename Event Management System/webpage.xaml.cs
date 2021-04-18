3d92ec4a1818779d3a7bd9e9b6daea3915be88a6
Akhil Kumar <akhilskumar1999@gmail.com>
2021-04-18 14:18:58 +0530

    webpage

---
+++ Event Management System/webpage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Event_Management_System
+{
+    /// <summary>
+    /// Interaction logic for webpage.xaml
+    /// </summary>
+    public partial class webpage : Window
+    {
+        string url;
+        public webpage(string _url)
+        {
+            InitializeComponent();
+            url = _url;
+        }
+    }
+}
